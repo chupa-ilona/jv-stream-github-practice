cd677372ac82796dc51c7cfa13217281759a5bad
IlonaChupa <ilonachupa@gmail.com>
2026-03-17 20:05:47 +0200

    done

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,22 +1,13 @@
 package practice;
 
 import java.util.*;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
 import model.Candidate;
 import model.Person;
 
 public class StreamPractice {
-    /**
-     * Given list of strings where each element contains 1+ numbers:
-     * input = {"5,30,100", "0,22,7", ...}
-     * return min integer value. One more thing - we're interested in even numbers.
-     * If there is no needed data throw RuntimeException with message
-     * "Can't get min value from list: < Here is our input 'numbers' >"
-     */
+
     public int findMinEvenNumber(List<String> numbers) {
         Stream<String> stream = numbers.stream();
         OptionalInt result = stream.map(s -> s.split(","))
@@ -27,28 +18,17 @@ public class StreamPractice {
         return result.orElseThrow(() -> new RuntimeException("Can't get min value from list: " + numbers));
     }
 
-    /**
-     * Given a List of Integer numbers,
-     * return the average of all odd numbers from the list or throw NoSuchElementException.
-     * But before that subtract 1 from each element on an odd position (having the odd index).
-     */
     public Double getOddNumsAverage(List<Integer> numbers) {
         for (int i = 1 ; i < numbers.size(); i = i + 2) {
             numbers.set(i, numbers.get(i) - 1);
         }
         Stream<Integer> stream = numbers.stream();
-        OptionalDouble result = stream.filter(i -> i % 2 != 0).mapToDouble(i -> i).average();
+        OptionalDouble result = stream.filter(i -> i % 2 != 0)
+                .mapToDouble(i -> i)
+                .average();
         return result.orElseThrow(()-> new NoSuchElementException("No odd numbers in the list"));
     }
 
-    /**
-     * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
-     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
-     * new Person(«Helen», 42, Sex.WOMAN))`,
-     * select from the List only men whose age is from `fromAge` to `toAge` inclusively.
-     * <p>
-     * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
-     */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         Stream<Person> stream = peopleList.stream();
 
@@ -57,21 +37,15 @@ public class StreamPractice {
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    /**
-     * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
-     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
-     * new Person(«Helen», 42, Sex.WOMAN))`,
-     * select from the List only people whose age is from `fromAge` and to `maleToAge` (for men)
-     * or to `femaleToAge` (for women) inclusively.
-     * <p>
-     * Example: select people of working age
-     * (from 18 y.o. and to 60 y.o. for men and to 55 y.o. for women inclusively).
-     */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         Stream<Person> stream = peopleList.stream();
-        return stream.filter(person -> (person.getSex() == Person.Sex.WOMAN && person.getAge() <= femaleToAge && person.getAge() >= fromAge)
-                        || (person.getSex() == Person.Sex.MAN && person.getAge() <= maleToAge && person.getAge() >= fromAge))
+        return stream.filter(person -> (person.getSex() == Person.Sex.WOMAN
+                        && person.getAge() <= femaleToAge
+                        && person.getAge() >= fromAge)
+                        || (person.getSex() == Person.Sex.MAN
+                        && person.getAge() <= maleToAge
+                        && person.getAge() >= fromAge))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
@@ -89,18 +63,6 @@ public class StreamPractice {
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    /**
-     * Your help with a election is needed. Given list of candidates, where each element
-     * has Candidate.class type.
-     * Check which candidates are eligible to apply for president position and return their
-     * names sorted alphabetically.
-     * The requirements are: person should be older than 35 years, should be allowed to vote,
-     * have nationality - 'Ukrainian'
-     * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
-     * which has following view: "2002-2015"
-     * We want to reuse our validation in future, so let's write our own impl of Predicate
-     * parametrized with Candidate in CandidateValidator.
-     */
     public List<String> validateCandidates(List<Candidate> candidates) {
         Stream<Candidate> stream = candidates.stream();
         return stream.filter(new CandidateValidator())
